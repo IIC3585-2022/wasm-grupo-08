da5165da1a33022b7b1272ae5b7bd74d877f2132
Tristan Heuer <tristan.heuer@gmail.com>
2022-04-19 19:24:37 -0400

    Work on partition

--- entrega-wasm/cpp/main.cpp
+++ entrega-wasm/cpp/main.cpp
@@ -1,14 +1,60 @@
 #include <emscripten/bind.h>
 #include <math.h>
+#include <cstdint>
+#include <vector>
+#include <iostream>
 
-int expTime(int n) {
-  int result = 0;
-  for (int i = 0; i < pow(2, n); i++) result++;
-  return result;
+using std::vector;
+using std::cout;
+
+typedef uint32_t u32;
+
+
+bool check_sums(const u32 *numbers, u32 size, u32 num_partitions, vector<u32> &asignment) {
+  auto sums = vector<u32>{num_partitions, 0};
+
+  for (u32 i = 0; i < size; i++) {
+    sums[asignment[i]] += numbers[i];
+  }
+
+  u32 sum = sums[0];
+  for (u32 i = 1; i < num_partitions; i++) {
+    if (sum != sums[i]) return false;
+  }
+
+  return true;
 }
-// emcc -lembind -o main.js main.cpp -s WASM_ASYNC_COMPILATION=0
-EMSCRIPTEN_BINDINGS(module) {
-    emscripten::function("expTime", &expTime);
+
+
+bool partition_rec(const u32 *numbers, u32 size, u32 num_partitions,vector<u32> &asignment, u32 position) {
+  // Base case
+  if (position == size) {
+    return check_sums(numbers, size, num_partitions, asignment);
+  }
+
+  for (u32 choice = 0; choice < num_partitions; choice++) {
+    asignment[position] = choice;
+    if (partition_rec(numbers, size, num_partitions, asignment, position+1)) return true;
+  }
+
+  return false;
 }
 
 
+extern "C" bool partition(u32 *numbers, u32 size, u32 num_partitions) {
+  if (num_partitions < 1) return false;
+  if (size < num_partitions) return false;
+
+  auto asignment = vector<u32>{size};
+
+  cout << "numbers: ";
+  for (u32 i = 0; i < size; i++) {
+    cout << numbers[i];
+    if (i < size-1) {
+      cout << ", ";
+    }
+  }
+  cout << std::endl;
+
+  return partition_rec(numbers, size, num_partitions, asignment, 0);
+}
--- entrega-wasm/js/index.js
+++ entrega-wasm/js/index.js
@@ -1,14 +1,60 @@
 const expTimeJS = (n) => {
   let result = 0;
-  for (let i = 0; i < Math.pow(2, n); i++) result++;
+  const limit = Math.pow(2, n);
+  for (let i = 0; i < limit; i++) result++;
   return result;
 };
 
+
+function check_sums(numbers, size, num_partitions, asignment) {
+  let sums = new Array(num_partitions).fill(0);
+
+  for (let i = 0; i < size; i++) {
+    sums[asignment[i]] += numbers[i];
+  }
+
+  const sum = sums[0];
+  for (let i = 1; i < num_partitions; i++) {
+    if (sum !== sums[i]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+
+function partition_rec(numbers, size, num_partitions, asignment, position) {
+  // Base case
+  if (position === size) {
+    return check_sums(numbers, size, num_partitions, asignment);
+  }
+
+  for (let choice = 0; choice < num_partitions; choice++) {
+    asignment[position] = choice;
+    if (partition_rec(numbers, size, num_partitions, asignment, position+1)) return true;
+  }
+
+  return false;
+}
+
+
+function partition(numbers, size, num_partitions) {
+  if (num_partitions < 1) return false;
+  if (size < num_partitions) return false;
+
+  let asignment = new Array(size);
+  return partition_rec(numbers, size, num_partitions, asignment, 0);
+}
+
+
 const getBenchmarkValue = () => {
-  return {
-    value: parseInt(document.getElementById('benchmark-value').value),
-  };
+  const length = document.getElementById('benchmark-value').value;
+  const min = 1;
+  const max = 1000;
+  return Uint32Array.from({length}, () => Math.floor(Math.random() * (max - min) + min))
 };
+
 
 const replaceValue = (which, value) => {
   document
@@ -16,24 +62,38 @@ const replaceValue = (which, value) => {
     .getElementsByClassName('value')[0].innerHTML = `${value}s`;
 };
 
+
 document.getElementById('js').onclick = () => {
-  const { value } = getBenchmarkValue();
+  let randomArray = getBenchmarkValue();
   const t0 = Date.now();
-  expTimeJS(value);
+  randomArray = Uint32Array.from([2, 2, 2]);
+  console.log(randomArray)
+  console.log(partition(randomArray, randomArray.length, 3));
   const t1 = Date.now();
   replaceValue('js', (t1 - t0) / 1000);
 };
 
+
 Module.onRuntimeInitialized = () => {
-  const { expTime } = Module;
+  const { partition } = Module;
   document.getElementById('wasm').onclick = () => {
-    const { value } = getBenchmarkValue();
+    let randomArray = getBenchmarkValue();
     const t0 = Date.now();
-    expTime(value);
+    randomArray = Uint32Array.from([2, 2, 2]);
+    const ptr = Module._malloc(randomArray.byteLength);
+    Module.HEAPU32.set(randomArray, ptr>>2);
+
+    result = Module.ccall("partition", "boolean",
+      ["number", "number", "number"],
+      [ptr, randomArray.length, 3]);
+
+    console.log(result);
+    Module._free(ptr);
     const t1 = Date.now();
     replaceValue('wasm', (t1 - t0) / 1000);
   };
 };
+
 
 const worker = new Worker('./js/worker.js');
 worker.onmessage = (e) => {
